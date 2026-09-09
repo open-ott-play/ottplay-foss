0eee90afe7077de0e6ecc62feea2c75257b0c36b
alvit <alvit.work+4alvit@gmail.com>
2026-09-08 18:11:48 -0700

    fix(scripts): MARKETING_VERSION bump sed matches full line

--- scripts/bump-mobile-version.sh
+++ scripts/bump-mobile-version.sh
@@ -11,7 +11,7 @@ sed -i.bak "s/versionCode [0-9]*/versionCode $VC/" android/app/build.gradle
 rm -f android/app/build.gradle.bak
 
 # iOS
-sed -i.bak "s/MARKETING_VERSION = [0-9.]*/MARKETING_VERSION = $VERSION;/g" ios/App/App.xcodeproj/project.pbxproj
+sed -i.bak "s/MARKETING_VERSION = .*/MARKETING_VERSION = $VERSION;/g" ios/App/App.xcodeproj/project.pbxproj
 sed -i.bak "s/CURRENT_PROJECT_VERSION = [0-9]*;/CURRENT_PROJECT_VERSION = $VC;/g" ios/App/App.xcodeproj/project.pbxproj
 rm -f ios/App/App.xcodeproj/project.pbxproj.bak
 
